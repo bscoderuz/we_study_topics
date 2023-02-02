ee65b6588d031eead9f2198430ace4add0ed19b2
bscoderuz <mrbscoder@gmail.com>
2023-02-02 06:25:23 -0800

    add gitignore

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('go_admin/', admin.site.urls),
     path('', include('base.urls')),
     path('users/', include('users.urls')),
     path('api/', include('api.urls')),
